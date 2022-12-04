38f414a2b55e028e5dff51feed6aeeec9b62b75f
Sara <yojan.bustamante@udea.edu.co>
2022-12-04 17:20:58 +0000

    updated menu

--- scripts/UI/Menu.gd
+++ scripts/UI/Menu.gd
@@ -7,7 +7,15 @@ onready var ground: = $GroundLayer/Ground
 var tween: SceneTreeTween
 
 func _ready () -> void:
+	randomize ()
+	var col: Color = Color (randf (), randf (), randf ())
+	background.modulate = col
+	ground.modulate = col
 	change_col ()
+
+func _process (delta: float) -> void:
+	ground.region_rect.position.x += 400 * delta
+	background.region_rect.position.x += 150 * delta
 
 func change_col () -> void:
 	if tween:
