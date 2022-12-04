df0dbeff817d3abd0d4ebab22c1fc636e49b0acd
Sara <yojan.bustamante@udea.edu.co>
2022-12-04 17:00:37 +0000

    refactored this

--- scripts/UI/Menu.gd
+++ scripts/UI/Menu.gd
@@ -4,18 +4,19 @@ extends Control
 onready var background: = $BackgroundLayer/Background
 onready var ground: = $GroundLayer/Ground
 
-var current_color: Color = Color.blue
-var other_color: Color = Color.magenta
-var color_mix: = 0.5
-var color_dir: = 1
+var tween: SceneTreeTween
 
-func _process (delta: float) -> void:
-	current_color = Color.linear_interpolate (Color.blend (other_color), color_mix)
-	if color_mix >= 1 or color_mix <= 0.4:
-		color_dir *= -1
-		randomize ()
-		other_color = Color (randf (), randf (), randf ())
-	color_mix += 0.0025 * color_dir
+func _ready () -> void:
+	change_col ()
+
+func change_col () -> void:
+	if tween:
+		tween.stop ()
 	
-	background.modulate = current_color
-	ground.modulate = current_color
+	randomize ()
+	tween = create_tween ().set_trans (Tween.TRANS_SINE)
+	var col: Color = Color (randf (), randf (), randf ())
+	tween.tween_property (background, "modulate", col, 5)
+	tween.parallel ().tween_property (ground, "modulate", col, 5)
+	yield (tween, "finished")
+	change_col ()
