fad7f938e648e0cf2057e0c2fd30f32afd4a408e
Sara <yojan.bustamante@udea.edu.co>
2022-12-10 21:15:35 +0000

    test

--- src/scripts/UI/Menu.gd
+++ src/scripts/UI/Menu.gd
@@ -1,4 +1,7 @@
 extends Control
+# TODO Play the menu song
+# Should there be an audio stream generated in PlayerData that has those looping songs,
+# and it's paused and resumen when entering and leaving a level respectively?
 
 onready var background: = $BackgroundLayer/Background
 onready var ground: = $GroundLayer/Ground
